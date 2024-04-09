7c6609768a8fdc8f0eb860ab9f244c80f97aa0c8
Omerfol <163458765+Omerfol@users.noreply.github.com>
2024-04-09 14:01:59 +0200

    Update AuthController.java
    #BugFix CrossOrgin improvement

--- Backend/src/main/java/com/ismb/meetingscheduler/Controllers/AuthController.java
+++ Backend/src/main/java/com/ismb/meetingscheduler/Controllers/AuthController.java
@@ -40,7 +40,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
