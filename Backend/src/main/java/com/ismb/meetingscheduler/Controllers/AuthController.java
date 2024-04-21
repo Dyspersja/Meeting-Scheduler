20195335c9acf1be5937ffa85f768b9a7abbadf7
Piotr Pecuch <piotr.pecuch@wp.pl>
2024-04-21 16:58:38 +0200

    0008-Fixing-refresh-token-overwriting

--- Backend/src/main/java/com/ismb/meetingscheduler/Controllers/AuthController.java
+++ Backend/src/main/java/com/ismb/meetingscheduler/Controllers/AuthController.java
@@ -24,6 +24,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -76,7 +77,7 @@ public class AuthController {
             String jwt = jwtUtils.generateJwtToken(userDetails.getEmail());
 
             List<String> roles = userDetails.getAuthorities().stream()
-                    .map(item -> item.getAuthority())
+                    .map(GrantedAuthority::getAuthority)
                     .collect(Collectors.toList());
 
             RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
