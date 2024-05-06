e6d28038c31cd915f71b8e45ad4066528e6d58af
Dyspersja <dyspersjamusic@gmail.com>
2024-05-06 23:57:35 +0200

    0015 Added endpoint for creating new meeting

---
+++ Backend/src/main/java/com/ismb/meetingscheduler/Controllers/MeetingController.java
@@ -0,0 +1,40 @@
+package com.ismb.meetingscheduler.Controllers;
+
+import com.ismb.meetingscheduler.Security.services.UserDetailsImpl;
+import com.ismb.meetingscheduler.models.Meeting;
+import com.ismb.meetingscheduler.models.User;
+import com.ismb.meetingscheduler.payload.Requests.MeetingRequest;
+import com.ismb.meetingscheduler.payload.Responses.MessageResponse;
+import com.ismb.meetingscheduler.repository.MeetingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/meeting")
+@RequiredArgsConstructor
+public class MeetingController {
+
+    private final MeetingRepository meetingRepository;
+
+    @PostMapping
+    public ResponseEntity<?> createMeeting(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody MeetingRequest request
+    ) {
+        Meeting meeting = Meeting.builder()
+                .title(request.getTitle())
+                .dateTime(request.getDateTime())
+                .description(request.getDescription())
+                .location(request.getLocation())
+                .organizerId(User.builder()
+                        .id(userDetails.getId())
+                        .build())
+                .build();
+
+        meetingRepository.save(meeting);
+        return ResponseEntity.ok(new MessageResponse("Meeting " + request.getTitle() + " created Successfully"));
+    }
+}
