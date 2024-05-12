dcded1a78606173babf4e0666464f1e24c5eab57
Dyspersja <dyspersjamusic@gmail.com>
2024-05-12 23:08:25 +0200

    0018 Added option to delete meeting

--- Backend/src/main/java/com/ismb/meetingscheduler/Controllers/MeetingController.java
+++ Backend/src/main/java/com/ismb/meetingscheduler/Controllers/MeetingController.java
@@ -7,9 +7,13 @@ import com.ismb.meetingscheduler.payload.Requests.MeetingRequest;
 import com.ismb.meetingscheduler.payload.Responses.MeetingResponse;
 import com.ismb.meetingscheduler.repository.MeetingRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -36,5 +40,23 @@ public class MeetingController {
 
         Meeting createdMeeting = meetingRepository.save(meeting);
         return ResponseEntity.ok(MeetingResponse.fromMeeting(createdMeeting));
+    }
+
+    @DeleteMapping("/{meetingId}")
+    public ResponseEntity<?> deleteMeeting(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable long meetingId
+    ) {
+        Optional<Meeting> optionalMeeting = meetingRepository.findById(meetingId);
+
+        if(optionalMeeting.isEmpty()) return ResponseEntity.notFound().build();
+
+        Meeting meeting = optionalMeeting.get();
+
+        if(!Objects.equals(meeting.getOrganizerId().getId(), userDetails.getId()))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not authorized to delete this meeting.");
+
+        meetingRepository.delete(meeting);
+        return ResponseEntity.noContent().build();
     }
 }
