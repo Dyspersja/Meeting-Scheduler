dc5327e8b70a5ceb7d4bfd20dd2f004e9b560714
Dyspersja <dyspersjamusic@gmail.com>
2024-05-12 23:34:39 +0200

    0018 Added option to invite attendees to meeting

--- Backend/src/main/java/com/ismb/meetingscheduler/Controllers/MeetingController.java
+++ Backend/src/main/java/com/ismb/meetingscheduler/Controllers/MeetingController.java
@@ -1,11 +1,16 @@
 package com.ismb.meetingscheduler.Controllers;
 
 import com.ismb.meetingscheduler.Security.services.UserDetailsImpl;
+import com.ismb.meetingscheduler.models.Attendee;
 import com.ismb.meetingscheduler.models.Meeting;
 import com.ismb.meetingscheduler.models.User;
+import com.ismb.meetingscheduler.payload.Requests.AttendeeRequest;
 import com.ismb.meetingscheduler.payload.Requests.MeetingRequest;
+import com.ismb.meetingscheduler.payload.Responses.AttendeeResponse;
 import com.ismb.meetingscheduler.payload.Responses.MeetingResponse;
+import com.ismb.meetingscheduler.repository.AttendeeRepository;
 import com.ismb.meetingscheduler.repository.MeetingRepository;
+import com.ismb.meetingscheduler.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +27,8 @@ import java.util.Optional;
 public class MeetingController {
 
     private final MeetingRepository meetingRepository;
+    private final UserRepository userRepository;
+    private final AttendeeRepository attendeeRepository;
 
     @PostMapping
     public ResponseEntity<MeetingResponse> createMeeting(
@@ -48,7 +55,6 @@ public class MeetingController {
             @PathVariable long meetingId
     ) {
         Optional<Meeting> optionalMeeting = meetingRepository.findById(meetingId);
-
         if(optionalMeeting.isEmpty()) return ResponseEntity.notFound().build();
 
         Meeting meeting = optionalMeeting.get();
@@ -58,5 +64,32 @@ public class MeetingController {
 
         meetingRepository.delete(meeting);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{meetingId}/attendee")
+    public ResponseEntity<AttendeeResponse> createMeeting(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable long meetingId,
+            @RequestBody AttendeeRequest attendeeRequest
+    ) {
+        Optional<Meeting> optionalMeeting = meetingRepository.findById(meetingId);
+        if(optionalMeeting.isEmpty()) return ResponseEntity.notFound().build();
+
+        Optional<User> optionalAttendee = userRepository.findByEmail(attendeeRequest.getEmail());
+        if(optionalAttendee.isEmpty()) return ResponseEntity.notFound().build();
+
+        Meeting meeting = optionalMeeting.get();
+        User user = optionalAttendee.get();
+
+        if(!Objects.equals(meeting.getOrganizerId().getId(), userDetails.getId()))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+
+        Attendee attendee = Attendee.builder()
+                .meetingId(meeting)
+                .userId(user)
+                .build();
+
+        Attendee newAttendee = attendeeRepository.save(attendee);
+        return ResponseEntity.ok(AttendeeResponse.fromAttendee(newAttendee));
     }
 }
