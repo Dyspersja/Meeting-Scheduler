0a17f084e710190ac2118e4035507fe1d8ab3ce7
Jagoda <jagoda.straszewska@gmail.com>
2024-05-20 17:59:27 +0200

    0021 Added UI for adding users to meeting

---
+++ Frontend/src/components/dashboard-components/addusermodal.component.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import MeetingService from '../../services/meeting.service';
+
+const AddUserModal = ({ showModal, onClose, meeting }) => {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (showModal) {
+            //tu coś ogarnąć
+        }
+    }, [showModal, meeting]);
+
+    const clearForm = () => {
+        setEmail('');
+        setError(null);
+    };
+
+
+    //to ogarnąć
+    
+    const handleSave = async () => {
+        try {
+            if (meeting) {
+                await MeetingService.addUserToMeeting(meeting.id, email);
+            }
+            onClose();
+        } catch (error) {
+            console.error('Wystąpił błąd:', error);
+            setError('Wystąpił błąd podczas zapisywania użytkownika.');
+        }
+    };
+
+    return (
+        <Modal show={showModal} onHide={onClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Dodaj użytkownika do spotkania</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {error && <div className="alert alert-danger" role="alert">{error}</div>}
+                <Form>
+                    <Form.Group controlId="formEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Podaj email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </Form.Group>
+
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onClose}>
+                    Anuluj
+                </Button>
+                <Button variant="primary" onClick={handleSave}>
+                    Zapisz
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default AddUserModal;
--- Frontend/src/components/dashboard-components/leftpanel.component.js
+++ Frontend/src/components/dashboard-components/leftpanel.component.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import authService from "../../services/auth.service";
-
+import AddEventModal from './addeventmodal.component';
+import AddUserModal from './addusermodal.component'; 
+import { FaEdit, FaTrash, FaPlus } from 'react-icons/fa'; 
+import MeetingService from '../../services/meeting.service';
 
 class LeftPanel extends Component {
     constructor(props) {
@@ -8,7 +11,10 @@ class LeftPanel extends Component {
         this.state = {
             meetings: [],
             tokenLoaded: false,
-            token: '' // Dodaj stan dla tokenu
+            token: '',
+            showEditModal: false,
+            showUserModal: false, 
+            selectedMeeting: null,
         };
     }
 
@@ -30,28 +36,88 @@ class LeftPanel extends Component {
         });
     }
 
+    openEditModal = (meeting) => {
+        this.setState({
+            showEditModal: true,
+            selectedMeeting: meeting,
+        });
+    }
+    
+    openAddUserModal = (meeting) => {
+        this.setState({
+            showUserModal: true,
+            selectedMeeting: meeting,
+        });
+    }
+    
+    closeModal = () => {
+        this.setState({
+            showEditModal: false,
+            selectedMeeting: null,
+        });
+    }
+    
+    closeUserModal = () => {
+        this.setState({
+            selectedMeeting: null,
+            showUserModal: false,
+        });
+    }
+
+    deleteMeeting = async (meetingId) => {
+        try {
+            await MeetingService.deleteMeeting(meetingId);
+            this.setState({ meetings: this.state.meetings.filter(meeting => meeting.id !== meetingId) });
+        } catch (error) {
+            console.error('Error deleting meeting:', error);
+        }
+    }
 
     render() {
         return (
             <div className='left-panel'>
                 <div className='add-button'>
-                    <button onClick={this.props.openModal}>Dodaj</button>
+                    <button onClick={this.openEditModal}>
+                        Dodaj
+                    </button>
                 </div>
                 <div className='planner'>
                     {Array.isArray(this.state.meetings) && this.state.meetings.map((item, index) => (
-                        <div key={index} className="meeting-item"
-                            style={{ backgroundColor: new Date(item.dateTime) < new Date() ? 'red' : 'green' }}>
+                        <div key={index} className={`meeting-item ${new Date(item.dateTime) < new Date() ? 'red' : 'green'}`}>
                             <p>Data i czas: {new Date(item.dateTime).toLocaleString()}</p>
                             <p>Opis: {item.description}</p>
                             <p>Lokalizacja: {item.location}</p>
                             <p>Organizator: {item.organizerId.email}</p>
+                            <button onClick={this.openAddUserModal} className="action-button add-user-button">
+                                <FaPlus />
+                            </button>
+                            <button onClick={() => this.openEditModal(item)} className="action-button edit-button">
+                                <FaEdit />
+                            </button>
+                            <button onClick={() => this.deleteMeeting(item.id)} className="action-button delete-button">
+                                <FaTrash />
+                            </button>
+
                         </div>
                     ))}
                 </div>
+                {this.state.showEditModal && 
+                    <AddEventModal 
+                        showModal={this.state.showEditModal} 
+                        onClose={this.closeModal} 
+                        meeting={this.state.selectedMeeting}
+                    />
+                }
+                {this.state.showUserModal &&  
+                    <AddUserModal 
+                        showModal={this.state.showUserModal} 
+                        onClose={this.closeUserModal} 
+                        meeting={this.state.selectedMeeting} 
+                    />
+                }
             </div>
         );
     }
 }
-
 
 export default LeftPanel;
