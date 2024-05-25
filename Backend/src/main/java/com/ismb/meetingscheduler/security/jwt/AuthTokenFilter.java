9a7bb72f020361f5f3e84b0718e4c9d0a7d09b8b
Dyspersja <dyspersjamusic@gmail.com>
2024-05-26 01:33:12 +0200

    0024 Bringed back AuthTokenFilter no args constructor

--- Backend/src/main/java/com/ismb/meetingscheduler/security/jwt/AuthTokenFilter.java
+++ Backend/src/main/java/com/ismb/meetingscheduler/security/jwt/AuthTokenFilter.java
@@ -5,8 +5,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,11 +17,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Slf4j
-@RequiredArgsConstructor
 public class AuthTokenFilter extends OncePerRequestFilter {
 
-    private final JwtUtils jwtUtils;
-    private final AuthenticatedUserService userDetailsService;
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private AuthenticatedUserService userDetailsService;
 
     @Override
     protected void doFilterInternal(
