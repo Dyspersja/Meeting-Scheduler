9421666fa9fa962f06b2cdd7cb484d67606a0070
Dyspersja <dyspersjamusic@gmail.com>
2024-05-26 12:08:52 +0200

    0027 Added Calendar export

--- Backend/src/main/java/com/ismb/meetingscheduler/calendar/CalendarController.java
+++ Backend/src/main/java/com/ismb/meetingscheduler/calendar/CalendarController.java
@@ -1,9 +1,19 @@
 package com.ismb.meetingscheduler.calendar;
 
+import com.ismb.meetingscheduler.meeting.model.Meeting;
+import com.ismb.meetingscheduler.meeting.payload.MeetingResponse;
+import com.ismb.meetingscheduler.meeting.repository.MeetingRepository;
+import com.ismb.meetingscheduler.security.auth.AuthenticatedUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -11,4 +21,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 public class CalendarController {
+
+    private final MeetingRepository meetingRepository;
+
+    @GetMapping
+    public ResponseEntity<String> exportCalendar(@AuthenticationPrincipal AuthenticatedUser userDetails) {
+        List<Meeting> organizerMeetingList = meetingRepository.findByOrganizerId(userDetails.getId());
+        List<Meeting> attendeeMeetingList = meetingRepository.findByAttendeeId(userDetails.getId());
+
+        List<MeetingResponse> meetingResponseList = new ArrayList<>();
+
+        meetingResponseList.addAll(organizerMeetingList.stream()
+                .map(meeting -> MeetingResponse.fromMeeting(meeting, true))
+                .toList());
+
+        meetingResponseList.addAll(attendeeMeetingList.stream()
+                .map(meeting -> MeetingResponse.fromMeeting(meeting, false))
+                .toList());
+
+        // Respond with 404 Not Found when not having any meetings
+        if (meetingResponseList.isEmpty()) return ResponseEntity.notFound().build();
+
+        CalendarContentBuilder calendarContentBuilder = CalendarContentBuilder.builder();
+        meetingResponseList.forEach(calendarContentBuilder::addMeeting);
+        String calendar = calendarContentBuilder.build();
+
+        return ResponseEntity.ok(calendar);
+    }
 }
